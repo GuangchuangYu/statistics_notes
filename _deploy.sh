709400788611466584c02b9e7149ca95820dce88
GuangchuangYu <guangchuangyu@gmail.com>
2016-04-11 12:01:31 +0800

    test deploy

--- _deploy.sh
+++ _deploy.sh
@@ -1,5 +1,8 @@
-# clone the repository to the book-output directory
 
+git config --global user.email "guangchuangyu@gmail.com"
+git config --global user.name "GuangchuangYu"
+
+# clone the repository to the book-output directory
 git clone -b gh-pages \
     https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git \
     book-output
